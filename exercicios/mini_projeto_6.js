ba0b0aff7459ac43b1041327da7fe73fce8c8cab
mayphias <lsoeu25@gmail.com>
2022-08-01 10:47:54 -0300

    conclusão do mini_p_6

--- exercicios/mini_projeto_6.js
+++ exercicios/mini_projeto_6.js
@@ -1,17 +1,28 @@
 //criar um metodo para ler as propriedades de um objeto
 //printar todas as propriedades do objeto que forem string
 
-const movie = {
+const movie = {        //criação do objeto e suas propriedades
     filme : "Inception",
     ano : 2010,
     atorPrincipal : "LeonardoDiCaprio"
 }
 
+function selectStrg(filme){   //função que imprime no console as partes das propriedades do obj que forem string
+    for (const property in filme) {   //pequeno loop para cada propriedade do obj
 
-for (const property in movie) {
-    if (typeof `${property}` == "string"){
-  console.log(`${property}: ${movie[property]}`);
+        if (IsJsonString(`${filme[property]}`)){  //se a função IsJsonString for verdadeira ele imprime no console
+        console.log(`${property}: ${filme[property]}`);  //a impressão no console
+        }
     }
 }
 
-//ainda em progresso...
+function IsJsonString(str) {  //função que testa se a propriedade do obj é uma string, retirado de: https://www.codegrepper.com/code-examples/javascript/javascript+check+if+object+is+string
+    try {                     // o programa tenta executar o codigo, se passar ele retorna verdadeiro no catch
+        JSON.parse(str);
+    } catch (e) {
+        return true;
+    }
+    return false;  //caso o codigo não de para ser executado ele retorna falso e o if na função selectStrg não é executado
+}
+
+selectStrg(movie);  //seleciona a função a ser executada e a variavel que ele vai trabalhar, no caso (movie)
